edcb2bb18e4cc6772f34746209ad444b5fac9c4d
hideki watabe <hideki.w.1971@gmail.com>
2022-07-23 17:47:54 +0900

    2022/07/23 17:46 swiper@8(zoomOut & navigation)を同一ページ内に独立した動きで実装できた。

--- js/script.js
+++ js/script.js
@@ -7,9 +7,21 @@ jQuery(function ($) {
     loop: true,
     speed: 2000,
     autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-      waitForTransition: false,
+      delay: 2500,
+      // disableOnInteraction: false,
+      // waitForTransition: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+    },
+  });
+
+  // swiper2
+  const swiper2 = new Swiper(".swiper2", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: ".swiper-pagination",
